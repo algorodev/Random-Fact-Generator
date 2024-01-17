1ecdeccc9b02b358f6767e087358ea8db455dbb9
Alejandro Gonzalez Romero <me@algorodev.com>
2024-01-17 19:55:01 +0100

    fix: build error

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,33 @@ const dailyButton = document.getElementById('daily-button')
 const randomButton = document.getElementById('random-button')
 const contentBox = document.getElementById('content')
 
-if (dailyButton && randomButton && contentBox) {
-	const quoteElement = await createQuoteElement('today')
-	contentBox.append(quoteElement)
-
-	dailyButton.addEventListener('click', async () => {
-		dailyButton.classList.add('tab__item--active')
-		randomButton.classList.remove('tab__item--active')
-
-		contentBox.replaceChildren()
+const app = async () => {
+	if (dailyButton && randomButton && contentBox) {
 		const quoteElement = await createQuoteElement('today')
 		contentBox.append(quoteElement)
-	})
 
-	randomButton.addEventListener('click', async () => {
-		dailyButton.classList.remove('tab__item--active')
-		randomButton.classList.add('tab__item--active')
+		dailyButton.addEventListener('click', async () => {
+			dailyButton.classList.add('tab__item--active')
+			randomButton.classList.remove('tab__item--active')
 
-		contentBox.replaceChildren()
-		const quoteElement = await createQuoteElement('random')
-		const refreshButton = createRefreshRandomFactButton(contentBox)
-		contentBox.append(quoteElement)
-		contentBox.append(refreshButton)
-	})
+			contentBox.replaceChildren()
+			const quoteElement = await createQuoteElement('today')
+			contentBox.append(quoteElement)
+		})
+
+		randomButton.addEventListener('click', async () => {
+			dailyButton.classList.remove('tab__item--active')
+			randomButton.classList.add('tab__item--active')
+
+			contentBox.replaceChildren()
+			const quoteElement = await createQuoteElement('random')
+			const refreshButton = createRefreshRandomFactButton(contentBox)
+			contentBox.append(quoteElement)
+			contentBox.append(refreshButton)
+		})
+	}
 }
+
+app()
+	.then(() => console.log('connected'))
+	.catch(error => console.error(error.message))
